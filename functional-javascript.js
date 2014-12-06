0546cf44c0fe4617e5b3ec053a7ab5bb9248d5fc
ElliotLewis8923 <elewis8923@gmail.com>
2014-12-06 14:04:39 +0000

    completed 8th task

--- functional-javascript.js
+++ functional-javascript.js
@@ -70,3 +70,17 @@ function reduce(arr, fn, initial) {
 }
 
 module.exports = reduce
+
+function duckCount() {
+	return Array.prototype.slice.call(arguments).filter(function(x) {
+		return Object.prototype.hasOwnProperty.call(x, 'quack');
+	}).length
+}
+
+module.exports = duckCount
+
+
+
+
+
+
